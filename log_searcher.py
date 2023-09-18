6475c12eb4c457b1f948a89a26ca23ccf5d016a4
Michael Nieto <mikensec@users.noreply.github.com>
2023-09-18 09:39:28 -0500

    update

--- log_searcher.py
+++ log_searcher.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import os
 import argparse
 import re
@@ -7,9 +8,13 @@ import json
 def search_files(directory, search_terms):
     results = []
 
+    print(f"Searching in directory: {directory}")
+    
     for foldername, subfolders, filenames in os.walk(directory):
         for filename in filenames:
             filepath = os.path.join(foldername, filename)
+
+            print(f"Reading file: {filepath}")
 
             with open(filepath, 'r', errors='replace') as file:
                 for line_num, line in enumerate(file, 1):
@@ -23,12 +28,14 @@ def search_files(directory, search_terms):
                             combined_search.extend(or_group)
 
                     if combined_search:
-                        results.append({
+                        match_data = {
                             "file": filepath,
                             "line_num": line_num,
                             "string": ' and '.join(combined_search),
                             "results": line.strip()
-                        })
+                        }
+                        results.append(match_data)
+                        print(f"Match found: {match_data}")
 
     return results
 
@@ -42,16 +49,20 @@ def main():
     # Split search terms by 'or', then each group by 'and'
     raw_search_terms = ' '.join(args.search_terms).split(' or ')
     search_terms = [group.split(' and ') for group in raw_search_terms]
+    
+    print(f"Parsed search terms: {search_terms}")
 
     results = search_files(args.directory, search_terms)
 
     if args.output:
         with open(args.output, 'w') as file:
             json.dump(results, file, indent=4)
+            print(f"Results saved to: {args.output}")
 
     # Printing to console
-    #for result in results:
-    #    print(json.dumps(result, indent=4))
+    for result in results:
+        print(json.dumps(result, indent=4))
 
 if __name__ == "__main__":
     main()
+                                                   
